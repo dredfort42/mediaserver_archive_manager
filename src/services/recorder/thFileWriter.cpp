89495c88722d19f5a6223f625cae718b07af033d
Dmitry Novikov <dredfort.42@gmail.com>
2025-10-21 18:14:55 +0100

    produce offset and rile write

--- src/services/recorder/thFileWriter.cpp
+++ src/services/recorder/thFileWriter.cpp
@@ -51,15 +51,17 @@ void writeAVPacketsToFile(Messenger *messenger,
             //                              std::atoi(filePath.c_str()),
             //                              std::atoi(currentFileName.c_str()));
 
-            // sendOffset(*messenger,
-            //            *topicForOffsets,
-            //            *cameraUUID,
-            //            packet.first,
-            //            offset,
-            //            std::atoi(filePath.c_str()),
-            //            std::atoi(currentFileName.c_str()));
-            fileName = std::to_string(ChronoName::getFileName(avPackets->front().first, *fragmentLengthInSeconds));
             //     thSendOffset.detach();
+
+            produceOffset(*messenger,
+                          *topicForOffsets,
+                          *cameraUUID,
+                          packet.first,
+                          offset,
+                          std::atoi(filePath.c_str()),
+                          std::atoi(currentFileName.c_str()));
+
+            fileName = std::to_string(ChronoName::getFileName(avPackets->front().first, *fragmentLengthInSeconds));
         }
 
         if (currentFileName != fileName && iFrame)
@@ -123,4 +125,33 @@ void writeAVPacketsToFile(Messenger *messenger,
         avFile.close();
 
     print(LogType::DEBUGER, "<<< Stop writer thread");
+}
+
+void produceOffset(Messenger *messenger,
+                   std::string topic,
+                   std::string cameraUUID,
+                   int64_t timestamp,
+                   int64_t offset,
+                   int folder,
+                   int file)
+{
+    if (DEBUG)
+        print(LogType::INFO, ">>> Start send offset thread");
+
+    proto::ProtoOffset protoOffset;
+    protoOffset.set_timestamp(timestamp);
+    protoOffset.set_offset(offset);
+    protoOffset.set_folder(folder);
+    protoOffset.set_file(file);
+
+    std::string offsetString;
+    protoOffset.SerializeToString(&offsetString);
+
+    messenger->produceMessage(topic, cameraUUID, offsetString);
+
+    if (DEBUG)
+    {
+        print(LogType::INFO, "Send offset: " + std::to_string(offset) + " for timestamp: " + std::to_string(timestamp));
+        print(LogType::INFO, "<<< Stop send offset thread");
+    }
 }
